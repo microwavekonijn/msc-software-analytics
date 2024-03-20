6f0974a8044878268294c9b1fe3c11824a669a27
Microwavekonijn <microwavekonijn@pm.me>
2024-03-20 11:37:05 +0100

    fix: log error + resolve promise

--- npm-miner/utils/retry.ts
+++ npm-miner/utils/retry.ts
@@ -1,4 +1,4 @@
-import {from, mergeMap, of, retry, Subject, tap, timer} from 'rxjs';
+import {from, map, mergeMap, of, retry, Subject, tap, timer} from 'rxjs';
 
 type Func<P extends Array<any>, R> = (...args: P) => Promise<R>;
 
@@ -10,10 +10,12 @@ export function retryWrapper<P extends Array<any>, R>(func: Func<P, R>): Func<P,
       mergeMap(req =>
         of(req).pipe(
           mergeMap(() => from(func(...req.args))),
-          tap({error: console.log}),
+          tap({error: err => console.log({req, err})}),
           retry({
             delay: (err, retryCount) => timer(Math.pow(1000, retryCount))
-          }))),
+          }),
+          map(res => req.resolve(res))
+        )),
     )
     .subscribe();
 
