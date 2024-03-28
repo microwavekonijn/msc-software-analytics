6a200fe8ba9d6984ef4cb6bcb34b1a3a36ae241f
Microwavekonijn <microwavekonijn@pm.me>
2024-03-28 13:10:50 +0100

    fix: ratelimit

--- npm-miner/utils/retry.ts
+++ npm-miner/utils/retry.ts
@@ -1,25 +1,29 @@
-import {from, map, mergeMap, of, retry, Subject, tap, timer} from 'rxjs';
+import {from, map, mergeMap, of, retry, Subject, tap, timer, catchError, EMPTY} from 'rxjs';
 
 type Func<P extends Array<any>, R> = (...args: P) => Promise<R>;
 
 export function retryWrapper<P extends Array<any>, R>(func: Func<P, R>): Func<P, R> {
-  const queue = new Subject<{ args: P, resolve: (R) => void }>();
+  const queue = new Subject<{ args: P, resolve: (R) => void, reject: (any) => void }>();
 
   queue
     .pipe(
       mergeMap(req =>
         of(req).pipe(
           mergeMap(() => from(func(...req.args))),
-          tap({error: err => console.log({req, err})}),
           retry({
-            delay: (err, retryCount) => timer(Math.pow(1000, retryCount))
+            count: 5,
+            delay: (err, retryCount) => timer(1000 * Math.pow(4, retryCount))
+          }),
+          catchError(err => {
+            req.reject(err);
+            return EMPTY;
           }),
           map(res => req.resolve(res))
         )),
     )
     .subscribe();
 
-  return (...args) => new Promise(resolve => {
-    queue.next({args, resolve});
+  return (...args) => new Promise((resolve, reject) => {
+    queue.next({args, resolve, reject});
   });
 }
