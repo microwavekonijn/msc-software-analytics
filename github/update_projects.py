eb93640a7a825f872b07c6a019c0c867fdef9162
Justtuchthat <61511231+Justtuchthat@users.noreply.github.com>
2024-03-30 11:10:08 +0100

    Now update_projects.py updates all projects that have not been processed yet

--- github/update_projects.py
+++ github/update_projects.py
@@ -18,6 +18,7 @@ headers = {
 
 def load_mongodb_config():
     config = configparser.ConfigParser()
+    # again cursed, but my computer hates me, and this is the only way it works for me
     config.read(os.getcwd() + '\\github\\config.ini')
     return {
         'host': config.get('mongodb', 'DB_HOST'),
@@ -35,6 +36,7 @@ def get_working_projects():
     client = MongoClient(mongo_uri)
     db = client[mdb_config['dbname']]
     collection = db.projects
+    # get all projects from db
     cursor = collection.find()
     return cursor
 
@@ -47,24 +49,28 @@ def get_owner_and_repo(repo_link):
         repo_link = repo_link[:-4]
     # now link is in format of 'https://github.com/<owner>/<repo>'
     owner_repo = repo_link[19:]
+    # all that is left is now '<owner>/<repo>'
     owner, repo = owner_repo.split('/')
     return owner, repo
 
 
 def get_delta_time(pull_request):
+    # very simple to get all time stamps
     time_created = datetime.strptime(pull_request["created_at"][:-1], "%Y-%m-%dT%H:%M:%S")
     time_merged = datetime.strptime(pull_request["merged_at"][:-1], "%Y-%m-%dT%H:%M:%S")
     return time_merged-time_created
 
 
-def update_project_with_time(project, avg_time):
+def update_project(project, avg_time, amount):
     mdb_config = load_mongodb_config()
     # MongoDB URI string
     mongo_uri = f"mongodb://{mdb_config['user']}:{mdb_config['password']}@{mdb_config['host']}:{mdb_config['port']}/{mdb_config['dbname']}?authSource=admin"
     client = MongoClient(mongo_uri)
     db = client[mdb_config['dbname']]
     collection = db.projects
+    # set the time and amount of pull requests correctly for a given project
     collection.update_many({"github": project["github"]}, {"$set": {"average_pull_request_merge_time": avg_time}})
+    collection.update_many({"github": project["github"]}, {"$set": {"amount_of_pull_requests": amount}})
 
 
 
@@ -75,38 +81,58 @@ def get_pull_requests(owner, repo):
     client = MongoClient(mongo_uri)
     db = client[mdb_config['dbname']]
     collection = db.pull_requests
+    # getting all pull requests where the project is from the same owner and from the right repo
     cursor = collection.find({"project": f"{owner}/{repo}"})
     return cursor
 
 
-
-def main():
-    # get the projects we want to work with
-    projects = get_working_projects()
-    # for each project, get the owner and repo
-    # then get pull requests
-    for project in projects:
-        owner, repo = get_owner_and_repo(project["github"])
-        pull_requests = get_pull_requests(owner, repo)
-        total = 0
-        time = 0
-        for request in pull_requests:
-            total += 1
-            time += get_delta_time(request).seconds
-        update_project_with_time(project, time//total)
-    
-
-def add_documentation_levels():
+def add_fields_to_document(project):
     mdb_config = load_mongodb_config()
     # MongoDB URI string
     mongo_uri = f"mongodb://{mdb_config['user']}:{mdb_config['password']}@{mdb_config['host']}:{mdb_config['port']}/{mdb_config['dbname']}?authSource=admin"
     client = MongoClient(mongo_uri)
     db = client[mdb_config['dbname']]
     collection = db.projects
-    collection.update_many({}, {"$set": {"amount_of_pull_requests": 0}})
+    # adding using seperate update_many funcs so they appear in the right order
+    # in python order does not matter, but in mongodb compass it does
+    collection.update_many({"github": project["github"]}, {"$set": {"average_pull_request_merge_time": 0}})
+    collection.update_many({"github": project["github"]}, {"$set": {"README_documentation": False,
+                                                                    "comments_in_code": False,
+                                                                    "website_linked": False,
+                                                                    "wiki_present": False}})
+    collection.update_many({"github": project["github"]}, {"$set": {"amount_of_pull_requests": 0}})
 
-    
+
+def main():
+    # get the projects we want to work with
+    projects = get_working_projects()
+
+    # for each project
+    for project in projects:
+        # if the project already is processed, skip it
+        if "README_documentation" in project.keys():
+            print("Skipping project ", project["github"])
+            continue
+
+        print("Working on ", project["github"])
+
+        # add the needed fields to the document
+        add_fields_to_document(project)
+
+        # get all pull requests
+        owner, repo = get_owner_and_repo(project["github"])
+        pull_requests = get_pull_requests(owner, repo)
+        total = 0
+        time = 0
+
+        # calculate how many pull requests in total, and what the average time is
+        for request in pull_requests:
+            total += 1
+            time += get_delta_time(request).seconds
+        
+        # update the project with average time and total amount of pull requests
+        update_project(project, time//total, total)
+
 
 if __name__ == "__main__":
-    # add_documentation_levels()
     main()
